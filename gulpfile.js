c56d8279e56c5ed8334c4ca1c53edd09b37a66d5
debasis778 <debasis778@gmail.com>
2018-05-01 19:38:56 +0530

    game

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+//compile scss to css and auto inject into browser
+gulp.task('sass',function(){
+	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss']).pipe(sass()).pipe(gulp.dest("src/css")).pipe(browserSync.stream());
+
+});
+//move js files from nodemodules to jquery folder
+
+gulp.task('js',function(){
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/tether/dist/js/tether.min.js']).pipe(gulp.dest("src/js")).pipe(browserSync.stream());
+
+});
+
+//static server +watching scss/html files
+gulp.task('serve',['sass'], function(){
+	browserSync.init({
+		server:"./src"
+	});
+	gulp.watch(['node_modules/bootstrap/scss/boostrap.scss','src/scss/*.scss'],['sass']);
+	gulp.watch("src/*.html").on('change',browserSync.reload);
+});
+gulp.task('default',['js','serve']);
